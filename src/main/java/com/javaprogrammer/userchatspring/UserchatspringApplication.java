ff031ee8a2ed730224382875bf66a673d7e1f604
Gevor <gevor8919@mail.ru>
2018-02-14 15:39:51 +0400

    Initial commit

---
+++ src/main/java/com/javaprogrammer/userchatspring/UserchatspringApplication.java
@@ -0,0 +1,12 @@
+package com.javaprogrammer.userchatspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserchatspringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserchatspringApplication.class, args);
+    }
+}
